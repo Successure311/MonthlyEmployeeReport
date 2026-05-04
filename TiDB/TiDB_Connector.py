ec1729133c51a26d4f1a9ce99fb24650893b3821
Admin <admin@successure.com>
2026-05-04 14:25:31 +0530

    Update reference credentials with new database name

--- TiDB/TiDB_Connector.py
+++ TiDB/TiDB_Connector.py
@@ -4,7 +4,7 @@ conn = mysql.connector.connect(
     host="gateway01.ap-southeast-1.prod.aws.tidbcloud.com",
     user="2hqkC7CYCyd33Y1.root",
     password="ED6Hesm31WIPzd8e",
-    database="test",
+    database="MonthlyEmployeeReport",
     port=4000,
     ssl_ca=r"C:\Sagar\BackTestCode\TiDB\isrgrootx1.pem"
 )
